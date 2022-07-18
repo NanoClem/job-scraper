9fa0072bba0af46206008fb9c2a26b3f215e3c28
NanoClem <clemdecoop.pro@gmail.com>
2022-07-18 21:06:33 +0200

    [feat] add get src path func

--- src/job_scraping/job_scraping/utils.py
+++ src/job_scraping/job_scraping/utils.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from http.cookies import SimpleCookie
 
 
@@ -6,3 +7,8 @@ def parse_cookie(raw_cookie: str) -> dict[str, str]:
     cookie_parser = SimpleCookie()
     cookie_parser.load(raw_cookie)
     return {k: v.value for k, v in cookie_parser.items()}
+
+
+def get_src_path() -> Path:
+    """Return the absolute path of the source folder."""
+    return (Path(__file__).parent.parent).resolve()
