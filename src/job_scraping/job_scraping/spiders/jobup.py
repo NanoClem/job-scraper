b7241aae71e296aeda95ba9d31aa3e41c0a5f758
NanoClem <clemdecoop.pro@gmail.com>
2022-07-22 02:55:04 +0200

    [feat] add custom pipelines

--- src/job_scraping/job_scraping/spiders/jobup.py
+++ src/job_scraping/job_scraping/spiders/jobup.py
@@ -1,18 +1,30 @@
 import json
+from typing import Optional
 
 import scrapy
 
 
 class JobupSpider(scrapy.Spider):
-    name = 'jobup'
-    allowed_domains = ['www.jobup.ch']
-    start_urls = ['https://www.jobup.ch/api/v1/public/search?location=Lausanne&query=data%20engineer&region-ids%5B%5D=37&rows=20']
-    page_index = 1
+    name: str = 'jobup'
+    allowed_domains: list[str] = ['www.jobup.ch']
+    start_urls: list[str] = [
+        'https://www.jobup.ch/api/v1/public/search?location=Lausanne&query=data%20engineer&region-ids%5B%5D=37&rows=20'
+    ]
+
+    custom_settings: Optional[dict] = {
+        'ITEM_PIPELINES': {
+            'job_scraping.pipelines.pipelines.JsonLoadingPipeline': 100,
+        }
+    }
+
+    page_index: int = 1
 
     def parse(self, response):
         data = json.loads(response.body)
         yield from data['documents']
-        
+
         if self.page_index < data['num_pages']:
             self.page_index += 1
-            yield scrapy.Request(url=f'{self.start_urls[0]}&page={self.page_index}', callback=self.parse)
+            yield scrapy.Request(
+                url=f'{self.start_urls[0]}&page={self.page_index}', callback=self.parse
+            )
