cc942b31f1e06f2af98615cfa485e43ea90b1ff2
NanoClem <clemdecoop.pro@gmail.com>
2022-07-22 02:55:57 +0200

    [feat] add pipelines and filter non useful data fields

--- src/job_scraping/job_scraping/spiders/academicwork.py
+++ src/job_scraping/job_scraping/spiders/academicwork.py
@@ -11,19 +11,23 @@ from job_scraping.items import AWItem
 
 class AcademicworkSpider(scrapy.Spider):
     name: str = 'academicwork'
-    allowed_domains: dict[str] = ['jobs.academicwork.ch']
-    start_urls: dict[str] = []
+    allowed_domains: list[str] = ['jobs.academicwork.ch']
+    start_urls: list[str] = []
 
     custom_settings: Optional[dict] = {
         'ITEM_PIPELINES': {
-            'job_scraping.pipelines.AWValidationPipeline': 100,
-            'job_scraping.pipelines.JsonLoadingPipeline': 200,
+            'job_scraping.pipelines.aw_pipelines.ValidationPipeline': 100,
+            'job_scraping.pipelines.aw_pipelines.JobUrlPipeline': 200,
+            'job_scraping.pipelines.pipelines.SnakeCasePipeline': 300,
+            'job_scraping.pipelines.pipelines.JsonLoadingPipeline': 400,
         }
     }
 
     curr_cookie: dict = {}
     curr_body: dict = {}
     config: Config = None
+    fields_filter: list[str] = ['JobCity','Requirements', 'ExtentOfWork', 'PublishDate', 'JobRef', 
+                                'Saved', 'Applied', 'NewLogoUrl', 'JobAdvertEntityId', 'JobTag']
 
     def make_request(self) -> Request:
         return Request(
@@ -44,7 +48,7 @@ class AcademicworkSpider(scrapy.Spider):
     def parse(self, response: Response):
         data = json.loads(response.body.decode('utf-8'))
         for job_add in data['Adverts']:
-            item = AWItem.construct(**job_add)
+            item = AWItem.construct(**{k: v for k, v in job_add.items() if k not in self.fields_filter})
             yield item.dict()
 
         if self.curr_body['StartIndex'] < data['TotalIndexes'] - 1:
