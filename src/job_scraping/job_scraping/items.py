7101fd38a6078d04ee3348ec085af5886d099237
NanoClem <clemdecoop.pro@gmail.com>
2022-07-22 02:55:32 +0200

    [update] remove non usefull fields

--- src/job_scraping/job_scraping/items.py
+++ src/job_scraping/job_scraping/items.py
@@ -5,22 +5,23 @@ from pydantic import BaseModel
 
 class AWItem(BaseModel):
     Id: int
-    JobAdvertEntityId: int
-    JobRef: int
+    # JobAdvertEntityId: int
+    # JobRef: int
+    JobUrl: str
     JobTitle: str
     Slug: str
     LeadIn: str
     Location: str
-    JobCity: str
+    # JobCity: str
     WorkExtent: str
     OrderType: str
     Category: str
     StartingDate: Any
-    JobTag: Optional[str]
-    Requirements: Any
+    # Requirements: Any
     CreatedDate: str
-    ExtentOfWork: Any
-    PublishDate: str
-    Saved: bool
-    Applied: bool
-    NewLogoUrl: str
+    # ExtentOfWork: Any
+    # PublishDate: str
+    JobUrl: str = ''
+    # Saved: bool
+    # Applied: bool
+    # NewLogoUrl: str
