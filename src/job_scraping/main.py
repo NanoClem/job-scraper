db92e4a78c1f740b58451d36fd2947421ee6a44b
NanoClem <clemdecoop.pro@gmail.com>
2022-08-01 15:41:50 +0200

    [update] run function now accept array of spiders

--- src/job_scraping/main.py
+++ src/job_scraping/main.py
@@ -1,4 +1,5 @@
 import logging
+import scrapy
 
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
@@ -8,26 +9,26 @@ from job_scraping.spiders import academicwork as aw, jobup as jbp
 from job_scraping.utils import get_src_path
 
 
-def run(runner: CrawlerRunner) -> None:
-    runner.crawl(aw.AcademicworkSpider)
-    runner.crawl(jbp.JobupSpider)
+def run(runner: CrawlerRunner, spiders: list[scrapy.Spider]) -> None:
+    for spd in spiders:
+        runner.crawl(spd)
+        
     d = runner.join()
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
 
 
 def main():
-    
     # Only with a runner (call configure_logging with a process)
     logging.basicConfig(
         filename=str(get_src_path() / 'scrap.log'),
-        level=logging.INFO,
+        level=logging.DEBUG,
         format='%(asctime)s [%(name)s] %(levelname)s: %(message)s',
         datefmt='%Y/%m/%d %H:%M:%S',
     )
- 
+
     runner = CrawlerRunner(get_project_settings())
-    run(runner)
+    run(runner, [aw.AcademicworkSpider])
 
 
 if __name__ == "__main__":
